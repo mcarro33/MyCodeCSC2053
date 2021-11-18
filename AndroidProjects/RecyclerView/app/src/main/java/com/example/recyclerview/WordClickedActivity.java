b7d7d2bcb3436165507238f157c024d9dc943354
mcarro33 <mcarro33@villanova.edu>
2021-11-18 10:51:24 -0500

    Added Recyclerview App

---
+++ AndroidProjects/RecyclerView/app/src/main/java/com/example/recyclerview/WordClickedActivity.java
@@ -0,0 +1,9 @@
+package com.example.recyclerview;
+
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WordClickedActivity extends AppCompatActivity {
+
+}
